4cb6eabd810562522f106d49498dddd994ed3af2
Breno Camilo <bcamilocontato@gmail.com>
2022-05-09 21:16:31 -0300

    Inserindo butão para topo

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
-// função para scroll e sua alterção de cor
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+
+// functions de rolagem
 function onScroll() {
+  showNavOnScroll()
+  showBackToTopButton()
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
   }
 }
-
+function showBackToTopButton() {
+  if (scrollY > 400) {
+    backtopreturn.classList.add('show')
+  } else {
+    backtopreturn.classList.remove('show')
+  }
+}
 // +++++++++++++++ funcions botoes do Header ++++++++++++++++++
 
 function OpenMenu() {
@@ -24,7 +39,15 @@ ScrollReveal({
 }).reveal(`
   #home, 
   #home img, 
-  #home .stats, 
+  #home .stats,
+  #home .stats .stat, 
   #services,
   #services header,
-  #services .card`)
+  #services .cards,
+  #services .cards .card,
+  #about, #about header,
+  #aobut .content,
+  #contato, #contato header,
+  #contato .content,
+  #footer,
+  `)
